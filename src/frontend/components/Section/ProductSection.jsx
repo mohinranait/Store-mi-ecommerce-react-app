d175313f1f21804cc872d6143f0c1989569f00de
Mohin Rana <ebrahimit49@gmail.com>
2023-10-24 15:30:31 +0600

    Product slider tab for desktop and dropdown menu for mobile with responsive

--- src/frontend/components/Section/ProductSection.jsx
+++ src/frontend/components/Section/ProductSection.jsx
@@ -2,11 +2,21 @@ import PropTypes from "prop-types"
 import { Swiper, SwiperSlide, } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import { HiMiniChevronDown } from "react-icons/hi2";
 // import required modules
 import { Navigation } from 'swiper/modules';
 import ProductCard from '../ProductCard/ProductCard';
+import { useState } from "react";
+
+
 
 const ProductSection = ({products}) => {
+    const [isMobileTab, setIsMobileTab] = useState(false);
+    const [selectCategory, setSelectCategory] = useState('Top Sell')
+    const handleCategory = (category) => {
+        setSelectCategory(category)
+        setIsMobileTab(!isMobileTab)
+    }
 
     return (
         <section className='py-7 bg-[#dcdcdc1a]'>
@@ -14,10 +24,14 @@ const ProductSection = ({products}) => {
                 <div className='flex justify-between bg-white px-4 items-center py-3 mb-3 border-b-2 border-gray-100 '>
                     <div className='text-lg font-semibold text-text-color '><span className='text-primary'>Features</span> Products</div>
                     <div className="relative">
-                        <ul className='flex items-center gap-4'>
-                            <li><a href="#" className='text-base text-primary font-semibold '>Top Sell</a></li>
-                            <li><a href="#" className='text-base text-text-color font-semibold'>Feature</a></li>
-                            <li><a href="#" className='text-base text-text-color font-semibold'>Active</a></li>
+                        <div onClick={() => setIsMobileTab(!isMobileTab)} className="lg:hidden flex items-center cursor-pointer gap-1">
+                            <span className=" text-base text-primary font-semibold">{selectCategory}</span>
+                            <span><HiMiniChevronDown /></span>
+                        </div>
+                        <ul className={`w-[150px] lg:w-auto  top-full right-0 z-10 bg-gray-50 lg:bg-transparent px-2 py-2 rounded-md lg:flex items-center gap-4 ${isMobileTab ? 'block absolute lg:static': 'hidden '}`}>
+                            <li><button onClick={() => handleCategory('Top Sell')} className={`text-base font-semibold ${selectCategory == 'Top Sell' ? 'text-primary':'text-text-color'} `}>Top Sell</button></li>
+                            <li><button onClick={() => handleCategory('Feature')} className={`text-base font-semibold ${selectCategory == 'Feature' ? 'text-primary':'text-text-color'} `}>Feature</button></li>
+                            <li><button onClick={() => handleCategory('Active')} className={`text-base font-semibold ${selectCategory == 'Active' ? 'text-primary':'text-text-color'} `}>Active</button></li>
                         </ul>
                     </div>
                 </div>
